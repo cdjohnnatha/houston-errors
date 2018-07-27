24b496c0036e0794f7273159cab377aa5d1ada29
claudio <cdjohnnatha@gmail.com>
2018-07-27 15:33:37 -0300

    remove routes folder, remove inputValidator

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
