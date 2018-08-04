2d3778a3391d019686581009587d856b5b970713
claudio <cdjohnnatha@gmail.com>
2018-08-04 19:17:22 -0300

    update http request errors client methods, added into index js and created test with jest

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const clientErrors = require('./lib/clientErrors');
+const httpRequestErrorsCore = require('./lib/httpRequestErrorsCore');
 
 module.exports =  {
     clientErrors,
+    httpRequestErrorsCore,
 };
