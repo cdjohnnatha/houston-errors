13948954748777078deb7a05cf4dec2d0d2ae0c7
claudio <cdjohnnatha@gmail.com>
2018-08-07 15:53:57 -0300

    fix wrong imports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const clientErrors = require('./lib/clientErrors');
-const httpRequestErrorsCore = require('./lib/httpRequestErrorsCore');
+const houston = require('./lib/houston');
+const houstonClientErrors = require('./lib/houstonClientErrors');
+const houstonServerErrors = require('./lib/houstonServerErrors');
 
 module.exports =  {
-    clientErrors,
-    httpRequestErrorsCore,
+    houston,
+    houstonClientErrors,
+    houstonServerErrors,
 };
