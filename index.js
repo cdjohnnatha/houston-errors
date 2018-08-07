32c58883a301df4822db709dcb30ff5ee7030a29
claudio <cdjohnnatha@gmail.com>
2018-08-07 16:30:09 -0300

    create houston modules which contains function for sequelize and jsonschema

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const houston = require('./lib/houston');
 const houstonClientErrors = require('./lib/houstonClientErrors');
 const houstonServerErrors = require('./lib/houstonServerErrors');
+const houstonModules = require('./lib/houstonModules');
 
 module.exports =  {
     houston,
     houstonClientErrors,
     houstonServerErrors,
+    houstonModules,
 };
