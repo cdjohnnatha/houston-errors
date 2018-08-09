603e50e682fdf138f887127bb536f71b94f9b846
claudio <cdjohnnatha@gmail.com>
2018-08-09 16:05:42 -0300

    create testes for houston

--- tests/houston.test.js
+++ tests/houston.test.js
@@ -5,6 +5,113 @@ const eventEmitter = new events();
 var emitter = require('events').EventEmitter;
 var em = new emitter();
 
+test('Test CleaningData', async () => {
+  const object = {name: null, code: undefined, error: 'Not Found'};
+  let cleaned = await houston.CleaningData(object);
+  expect(object).not.toHaveProperty('name');
+  expect(object).not.toHaveProperty('code');
+  expect(object).toHaveProperty('error', 'Not Found');
+});
+
+test('Test DefaultError', async () => {
+  const sendMessage = 'This is Houston. Say again, please';
+  let defaultError = await houston.DefaultError(houstonClientErrors.BAD_REQUEST, {
+    message: sendMessage,
+    data: '{Lousma: houston, we have a problem.}',
+  });
+  expect(defaultError).toHaveProperty('name', 'BAD_REQUEST');
+  expect(defaultError).toHaveProperty('code', 400);
+  expect(defaultError).toHaveProperty('error', 'Bad Request');
+  expect(defaultError).toHaveProperty('message', sendMessage);
+  expect(defaultError).toHaveProperty('data', '{Lousma: houston, we have a problem.}');
+
+  defaultError = await houston.DefaultError(houstonClientErrors.BAD_REQUEST, {
+    message: sendMessage,
+  });
+  expect(defaultError).toHaveProperty('name', 'BAD_REQUEST');
+  expect(defaultError).toHaveProperty('code', 400);
+  expect(defaultError).toHaveProperty('error', 'Bad Request');
+  expect(defaultError).toHaveProperty('message', sendMessage);
+  expect(defaultError).not.toHaveProperty('data');
+
+  defaultError = await houston.DefaultError(houstonClientErrors.BAD_REQUEST, {});
+  expect(defaultError).toHaveProperty('name', 'BAD_REQUEST');
+  expect(defaultError).toHaveProperty('code', 400);
+  expect(defaultError).toHaveProperty('error', 'Bad Request');
+  expect(defaultError).not.toHaveProperty('data');
+});
+
+
+test('Test CustomError', async () => {
+  const sendMessage = 'This is Houston. Say again, please';
+  const lovellData = {lovell: 'We have had a MAIN B BUS UNDERVOLT'};
+  const errorDef = 'houston, we have a problem';
+  let customError = await houston.CustomError({
+    code: 406,
+    error: errorDef,
+    name:'Lousma',
+    message: sendMessage,
+    data: lovellData,
+  });
+  expect(customError).toHaveProperty('name', 'Lousma');
+  expect(customError).toHaveProperty('code', 406);
+  expect(customError).toHaveProperty('error', errorDef);
+  expect(customError).toHaveProperty('message', sendMessage);
+  expect(customError).toHaveProperty('data', lovellData);
+
+  customError = await houston.CustomError({
+    error: errorDef,
+    name:'Lousma',
+    message: sendMessage,
+    data: lovellData,
+  });
+  expect(customError).toHaveProperty('code', 500);
+  expect(customError).toHaveProperty('name', 'Lousma');
+  expect(customError).toHaveProperty('error', errorDef);
+  expect(customError).toHaveProperty('message', sendMessage);
+  expect(customError).toHaveProperty('data', lovellData);
+
+  customError = await houston.CustomError({
+    name:'Lousma',
+    message: sendMessage,
+    data: lovellData,
+  });
+  expect(customError).toHaveProperty('code', 500);
+  expect(customError).toHaveProperty('error', 'Internal Server Error');
+  expect(customError).toHaveProperty('name', 'Lousma');
+  expect(customError).toHaveProperty('message', sendMessage);
+  expect(customError).toHaveProperty('data', lovellData);
+
+  customError = await houston.CustomError({
+    message: sendMessage,
+    data: lovellData,
+  });
+  expect(customError).toHaveProperty('code', 500);
+  expect(customError).toHaveProperty('error', 'Internal Server Error');
+  expect(customError).toHaveProperty('name', 'INTERNAL_SERVER_ERROR');
+  expect(customError).toHaveProperty('message', sendMessage);
+  expect(customError).toHaveProperty('data', lovellData);
+
+  customError = await houston.CustomError({
+    data: lovellData,
+  });
+
+  expect(customError).toHaveProperty('code', 500);
+  expect(customError).toHaveProperty('error', 'Internal Server Error');
+  expect(customError).toHaveProperty('name', 'INTERNAL_SERVER_ERROR');
+  expect(customError).toHaveProperty('message', '');
+  expect(customError).toHaveProperty('data', lovellData);
+
+  customError = await houston.CustomError({});
+  expect(customError).toHaveProperty('code', 500);
+  expect(customError).toHaveProperty('error', 'Internal Server Error');
+  expect(customError).toHaveProperty('name', 'INTERNAL_SERVER_ERROR');
+  expect(customError).toHaveProperty('message', '');
+  expect(customError).not.toHaveProperty('data');
+});
+
+
+
 test('Test HoustonErrorEvent', async () => {
   const customMessage = 'custom message';
   const data = {message: undefined, data: customMessage};
@@ -16,23 +123,4 @@ test('Test HoustonErrorEvent', async () => {
     expect(error.data).toBeUndefined();
   });
   await houstonClientErrors.BadRequest();
-});
-
-test('Test CleaningData', async () => {
-  const object = {name: null, code: undefined, error: 'Not Found'};
-  let cleaned = await houston.CleaningData(object);
-  expect(object).not.toHaveProperty('name');
-  expect(object).not.toHaveProperty('code');
-  expect(object).toHaveProperty('error', 'Not Found');
-});
-
-test('Test DefaultError', async () => {
-  const defaultError = await houston.DefaultError(houstonClientErrors.BAD_REQUEST,{
-    message: 'test',
-    data: '{data: houston, we have a problem.}',
-  })
-  expect(defaultError).not.toHaveProperty('name', 'BAD_REQUEST');
-  expect(object).not.toHaveProperty('code', 400);
-  expect(object).toHaveProperty('error', 'Bad Request');
-  expect(object).toHaveProperty('data', 'houston, we have a problem');
 });
